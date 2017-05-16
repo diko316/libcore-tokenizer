d57d626e370a0fdab848b2e841fe8ec1351e5d30
Diko Tech Slave <diko316@gmail.com>
2017-05-16 18:28:24 +0800

    added partial regex basic test

---
+++ src/test/regex/basic.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+
+it("1. Should define tokens based from String name and " +
+        " RegExp regular expression",
+    function () {
+        
+        
+        
+        
+    });
